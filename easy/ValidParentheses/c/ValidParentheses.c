21ccfb8a03bf592b131a36f74d7f04f70f5f4db9
Pete Richardson <pete@peterichardson.com>
2022-12-29 13:25:21 -0800

    20. Valid Parentheses (easy) - C. stack version.   Slower than non-stack version.

--- easy/ValidParentheses/c/ValidParentheses.c
+++ easy/ValidParentheses/c/ValidParentheses.c
@@ -4,17 +4,9 @@
 #include <string.h>
 #include <assert.h>
 
-bool isBracket(char c) {
-    // true iff c is an opening bracket
-    return c == '[' || c == ']' || c == '{' || c == '}' || c == '(' || c == ')';
-}
 bool isLeft(char c) {
     // true iff c is an opening bracket
     return c == '[' || c == '{' || c == '(';
-}
-bool isRight(char c) {
-    // true iff c is a closing bracket
-    return c == ']' || c == '}' || c == ')';
 }
 bool isMatch(char l, char r) {
     // true iff r is the proper closing bracket for l
@@ -23,71 +15,74 @@ bool isMatch(char l, char r) {
             ((l == '(') && (r == ')'));
 }
 
-bool isValid(char * input){
-    char *s = strdup(input);
-    printf("\tbefore: '%s'\n", s);
-
-    char* trailing = s;
-    if (*trailing == '\0')
-        return true;
-    char* leading = s+1;
-    if (*leading == '\0' || isRight(*trailing))
-        return false;
-    for (leading = s+1; *leading != '\0'; leading++) {
-        printf("\ttrailing = '%c', leading = '%c'", *trailing, *leading);
-        assert(isBracket(*leading));
-        if (isMatch(*trailing, *leading)) {
-            printf(" MATCH! removing both.");
-            *trailing = '.';
-            while (trailing > s && *trailing == '.')
-                trailing--;
-            *leading = '.';
-        };
-        if (isLeft(*leading)) {
-            trailing = leading;
-            printf(" do nothing.");
-        }
-        if (isRight(*leading)) {
-            // right but no match!  FAIL.
-            printf(" invalid: unmatched right\n");
-            return false;
-        }
-        printf("\n");
-    }
-    printf("\tafter:  '%s'\n", s);
-    printf("\ttrailing = '%c', leading = '%c'\n", *trailing, *leading);
-    printf("\t&s = 0x%X, &trailing = 0x%X, &leading = 0x%X\n", s, trailing, leading);
-
-    if (*trailing != '\.') {
-        printf("\tSUCKAGE - trailing = '%s', &trailing = 0x%X, &leading = 0x%X\n", trailing, trailing, s);
+bool isValid(char * s){
+    printf("\ts = '%s'\n", s);
+    size_t l = strlen(s);
+    if (l % 2 == 1) {
+        printf("\t\t odd length (%zu)\n", l);
         return false;
     }
-    // Last chance... make sure length is even.
-    // return ((leading - trailing) %2) == 0;
-    return true;
+    
+    size_t max_stack_size = (l/2)+1;
+    // max stack needed is 1/2 the size of the input string.
+    char* stack = malloc(sizeof(char) * max_stack_size);
+    int top = 0;
+
+    // walk the string, pushing and popping
+    for (int i = 0; i < l; i++) {
+        char c = s[i];
+        printf("\t%03d. '%c', top = %d -> stack before = %s\n", i, c, top, stack);
+
+        if (isLeft(c)) {    // push
+            if ((top + 1) > max_stack_size) {
+                printf("\t\tNested too deep. Can't recover.\n");
+                if (stack) free(stack);
+                return false;
+            }
+            // printf("\t\tPushing '%c' onto stack. top = %d, stack[top] = '%c'\n", c, top, stack[top]);
+            stack[top++] = c;
+            printf("\t\tPushed  '%c' onto stack. top = %d, stack[top-1] = '%c'\n", c, top, stack[top-1]);
+        } else {
+            printf("\t\tsaw a RIGHT: c = '%c', top-1 = %d, stack[top-1] = '%c'\n", c, top-1, stack[top-1]);
+            if (top == 0 || !isMatch(stack[top-1], c)) {
+                if (stack) free(stack);
+                return false;
+            }
+            printf("\t\tMATCH! c = '%c', stack[%int - 1] = '%c'\n", c, top, stack[top-1]);
+             top--;
+        }
+        printf("\t\t-> stack after = %s\n", stack);
+
+    }
+    if (stack) free(stack);
+    return top == 0;
 }
 
 void test(char *s, bool expected, char* name) {
     printf("test '%s'  '%s' - \n", name, s);
     bool result = isValid(s);
+    printf("\tresult = %s, expected = %s\n", result ? "valid" : "invalid", expected ? "valid" : "invalid");
     printf (result == expected ? "\tpass\n" : "\tFAIL\n");
 }
 
 int main(int argc, char**argv) {
-    test("()[]{}", true, "leet example 2");
+    test("", true, "empty string");
+    test("[", false, "single left");
+    test("]", false, "single right");
     test("()", true, "leet example 1");
+    test("){", false, "leet example 9");
+    test("([}}])", false, "leet example 92");
+    test("[]", true, "simple");
     test("(]", false, "leet example 3");
+    test("()[]{}", true, "leet example 2");
     test(")(){}", false, "leet example 89");
     test("([]", false, "leet example 90");
     test("(([]", false, "leet example 90.1");
-    test("[]", true, "simple");
-    test("[", false, "single left");
-    test("]", false, "single right");
     test("[][", false, "odd length > 1");
     test("[[", false, "odd length > 1");
-    test("", true, "empty string");
     test("[]{}", true, "serial");
     test("[{}]", true, "nested");
     test("[{]}", false, "overlapping");
     test("[{}()]", true, "nested serial");
+    test("[[[[[[", false, "nested too deep");
 }
